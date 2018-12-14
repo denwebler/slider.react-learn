c9464e64e0623d16776209160fec2921a75de3d5
Denis Hr <den.webler@yandex.ru>
2018-12-14 23:28:43 +0200

    My first commit

--- src/App.js
+++ src/App.js
@@ -2,27 +2,29 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import styled from 'styled-components'
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className="wrapper">
+          <SayHello hello="Hello" anything="World," guys="People!" smile=":D" linkMe="https://www.instagram.com/denis.hrudin/" linkReact="https://reactjs.org/docs/create-a-new-react-app.html"/>
+        </div>
     );
   }
+}
+
+function SayHello(props) {
+  return (
+      <div>
+        <h1> {props.hello} {props.anything} {props.guys} {props.smile}</h1>
+        <a href={props.linkMe}> Created by Denis Hrudin </a>
+        <a href={props.linkReact}> on the basis of React.js </a>
+      </div>
+    )
 }
 
 export default App;
